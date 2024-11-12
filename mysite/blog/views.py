1dabe9bf5fe4cc3cf9b62d18bd05ca527dc8bc17
SeleminUriy <prepress-s@selemin.ru>
2024-11-12 22:04:43 +0300

    Commit_12.11.20:43
    Signed-off-by: SeleminUriy <prepress-s@selemin.ru>

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,13 +4,16 @@ from .models import Post
 
 
 def post_list(request):
-    # получаем все посты
+    # Get all posts
     posts = Post.objects.all()
 
-    # создаем пагинатор
-    paginator = Paginator(posts, 10)  # 10 постов на странице
+    # Get the selected number of items per page from the request
+    items_per_page = int(request.GET.get('items_per_page', 10))
 
-    # получаем номер страницы, на которую переходит пользователь
+    # Create a paginator with the selected number of items per page
+    paginator = Paginator(posts, items_per_page)
+
+    # Get the page number from the request
     page_number = request.GET.get('page')
     try:
         page_posts = paginator.get_page(page_number)
@@ -19,4 +22,5 @@ def post_list(request):
     except EmptyPage:
         page_posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog/post_list.html', {'page_posts': page_posts})
+    return render(request, 'blog/post_list.html',
+                                {'page_posts': page_posts, 'items_per_page': items_per_page})
