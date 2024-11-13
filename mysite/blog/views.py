10f2f2280ef5ecea902cd7ac1e5198eb87f1ba05
Selemin_Uriy <prepress-s@selemin.ru>
2024-11-13 17:16:08 +0300

    Add Page 2/3/4/5
    Signed-off-by: Selemin_Uriy <prepress-s@selemin.ru>

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,116 +2,33 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Post
 
+def paginated_view(request, page_number, template_name):
+    posts = Post.objects.all()
+
+    items_per_page = int(request.GET.get('items_per_page', 10))
+
+    paginator = Paginator(posts, items_per_page)
+
+    try:
+        page_posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        page_posts = paginator.page(1)
+    except EmptyPage:
+        page_posts = paginator.page(paginator.num_pages)
+
+    return render(request, template_name, {'page_posts': page_posts, 'items_per_page': items_per_page})
 
 def post_list(request):
-    # Get all posts
-    posts = Post.objects.all()
-
-    # Get the selected number of items per page from the request
-    items_per_page = int(request.GET.get('items_per_page', 10))
-
-    # Create a paginator with the selected number of items per page
-    paginator = Paginator(posts, items_per_page)
-
-    # Get the page number from the request
-    page_number = request.GET.get('page')
-    try:
-        page_posts = paginator.get_page(page_number)
-    except PageNotAnInteger:
-        page_posts = paginator.page(1)
-    except EmptyPage:
-        page_posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post_list.html',
-                                {'page_posts': page_posts, 'items_per_page': items_per_page})
+    return paginated_view(request, 1, 'blog/post_list.html')
 
 def page2(request):
-    # Get all posts
-    posts = Post.objects.all()
-
-    # Get the selected number of items per page from the request
-    items_per_page = int(request.GET.get('items_per_page', 10))
-
-    # Create a paginator with the selected number of items per page
-    paginator = Paginator(posts, items_per_page)
-
-    # Get the page number from the request
-    page_number = request.GET.get('page')
-    try:
-        page_posts = paginator.get_page(page_number)
-    except PageNotAnInteger:
-        page_posts = paginator.page(1)
-    except EmptyPage:
-        page_posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/page2.html',
-                                {'page_posts': page_posts, 'items_per_page': items_per_page})
+    return paginated_view(request, 2, 'blog/page2.html')
 
 def page3(request):
-    # Get all posts
-    posts = Post.objects.all()
-
-    # Get the selected number of items per page from the request
-    items_per_page = int(request.GET.get('items_per_page', 10))
-
-    # Create a paginator with the selected number of items per page
-    paginator = Paginator(posts, items_per_page)
-
-    # Get the page number from the request
-    page_number = request.GET.get('page')
-    try:
-        page_posts = paginator.get_page(page_number)
-    except PageNotAnInteger:
-        page_posts = paginator.page(1)
-    except EmptyPage:
-        page_posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/page3.html',
-                                {'page_posts': page_posts, 'items_per_page': items_per_page})
+    return paginated_view(request, 3, 'blog/page3.html')
 
 def page4(request):
-    # Get all posts
-    posts = Post.objects.all()
-
-    # Get the selected number of items per page from the request
-    items_per_page = int(request.GET.get('items_per_page', 10))
-
-    # Create a paginator with the selected number of items per page
-    paginator = Paginator(posts, items_per_page)
-
-    # Get the page number from the request
-    page_number = request.GET.get('page')
-    try:
-        page_posts = paginator.get_page(page_number)
-    except PageNotAnInteger:
-        page_posts = paginator.page(1)
-    except EmptyPage:
-        page_posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/page4.html',
-                                {'page_posts': page_posts, 'items_per_page': items_per_page})
+    return paginated_view(request, 4, 'blog/page4.html')
 
 def page5(request):
-    # Get all posts
-    posts = Post.objects.all()
-
-    # Get the selected number of items per page from the request
-    items_per_page = int(request.GET.get('items_per_page', 10))
-
-    # Create a paginator with the selected number of items per page
-    paginator = Paginator(posts, items_per_page)
-
-    # Get the page number from the request
-    page_number = request.GET.get('page')
-    try:
-        page_posts = paginator.get_page(page_number)
-    except PageNotAnInteger:
-        page_posts = paginator.page(1)
-    except EmptyPage:
-        page_posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/page5.html',
-                                {'page_posts': page_posts, 'items_per_page': items_per_page})
-
-
-
+    return paginated_view(request, 5, 'blog/page5.html')
