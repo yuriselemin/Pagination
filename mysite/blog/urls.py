d6686843433a2b111f613d2d3e6e606ad32556e5
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-11-14 00:47:57 +0300

    Update urls.py

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from .views import post_list
 
 urlpatterns = [
-    path('', post_list, name='post_list')
-#     path('page2/', page2, name='page2'),
-#     path('page3/', page3, name='page3'),
-#     path('page4/', page4, name='page4'),
-#     path('page5/', page5, name='page5'),
+    path('', post_list, name='post_list'),
+
 ]
 
 
