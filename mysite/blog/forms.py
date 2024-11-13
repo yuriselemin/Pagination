d72808ec1d1b583c073e801b5ac17e81f86d5e7b
SeleminUriy <prepress-s@selemin.ru>
2024-11-14 01:36:00 +0300

    Add CSS
    Signed-off-by: SeleminUriy <prepress-s@selemin.ru>

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -4,5 +4,5 @@ class ItemsPerPageForm(forms.Form):
     items_per_page = forms.ChoiceField(label="Постов на странице",
         choices=[('3', '3'), ('6', '6'), ('10', '10')],
         widget=forms.Select(attrs={'onchange': 'this.form.submit()'}),
-        initial='3'
+        initial='2'
     )
